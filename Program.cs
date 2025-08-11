f3a7258bdd3b052d682ac2907c1d48d154ddac06
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-08-11 13:53:10 -0300

    Resolução do deasio trilha-net-poo, utilizando todos os conhecimentos sobre programação orientada a objetos.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Instânciando um objeto da classe Nokia
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123456789", "N98", "5555555", 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("-------------------------");
+
+// Instânciando um objeto da classe Iphone
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("247856987", "11", "4444444", 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
